649ddd2cfca675d22187d5abe983cea3c9b2b7bb
Aaron Keisler <amkeisler@gmail.com>
2019-05-16 00:54:26 -0400

    Update JS and HTML

--- 02-Homework/Psychic-Game/assets/javascript/psychic.js
+++ 02-Homework/Psychic-Game/assets/javascript/psychic.js
@@ -2,6 +2,21 @@ let wins = 0;
 let losses = 0;
 let guessesLeft = 10;
 let guessesUsed = 0;
+let guess;
+let winText = document.querySelector("#wins");
+let lossText = document.querySelector("#losses");
+let guessLeftText = document.querySelector("#guessesLeft");
+let guessUsedText = document.querySelector("#guessesUsed");
+let jumboText = document.querySelector("#jumboText");
+let ltrInput = document
+  .querySelector("#inputLetter")
+  .addEventListener("keyup", getKey);
+let ltrFocus = document
+  .querySelector("#inputLetter")
+  .addEventListener("focus", gameInstruct);
+let ltrBlur = document
+  .querySelector("#inputLetter")
+  .addEventListener("blur", instructAway);
 const letters = [
   "A",
   "B",
@@ -31,23 +46,42 @@ const letters = [
   "Z"
 ];
 
-const randomLetter = () => Math.floor(Math.random() * 26);
-
-let targetLetter = letters[randomLetter()];
-
-function game() {
-  for (let i = 0; i < guessesLeft; i++) {
-    let guess = prompt("What letter?");
-    console.log(guessesUsed);
-    if (String.toString(guess) === targetLetter) {
-      wins++;
-      alert("You won!");
-      break;
-    } else {
-      guessesUsed++;
-      alert("Try again!");
-    }
+function gameInstruct() {
+  if (guessesLeft > 1) {
+    jumboText.textContent = "Please enter a letter below!";
+  } else {
+    jumboText.textContent = "GAME OVER!";
   }
 }
 
-alert("guesses used up!");
+function instructAway() {
+  jumboText.textContent = "";
+}
+
+function getKey(e) {
+  guess = e.key;
+  e.target.value = "";
+  game();
+}
+function game() {
+  if (guessesLeft > 1) {
+    let targetLetter = letters[Math.floor(Math.random() * 26)];
+    if (guess.toUpperCase() === targetLetter) {
+      wins++;
+      winText.textContent = wins;
+      jumboText.textContent = "You won!";
+    } else {
+      losses++;
+      guessesUsed++;
+      guessesLeft--;
+      jumboText.textContent = `OOPS! The correct guess would have been ${targetLetter}`;
+      lossText.textContent = losses;
+      guessUsedText.textContent = guessesUsed;
+      guessLeftText.textContent = guessesLeft;
+    }
+  } else {
+    jumboText.textContent = "GAME OVER!";
+    document.querySelector("#stats").style.display = "none";
+    // document.querySelector("#inputLetter").setAttribute("disabled", "");
+  }
+}
