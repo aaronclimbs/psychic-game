11b54552a5016bab4933ec654cacf79eb7d35097
Aaron Keisler <amkeisler@gmail.com>
2019-05-16 10:46:47 -0400

    refined JS; added new game functionality

--- 02-Homework/Psychic-Game/assets/javascript/psychic.js
+++ 02-Homework/Psychic-Game/assets/javascript/psychic.js
@@ -1,13 +1,19 @@
+// declare variables and set initial counts
 let wins = 0;
 let losses = 0;
 let guessesLeft = 10;
 let guessesUsed = 0;
 let guess;
+
+// grab IDs
 let winText = document.querySelector("#wins");
 let lossText = document.querySelector("#losses");
 let guessLeftText = document.querySelector("#guessesLeft");
 let guessUsedText = document.querySelector("#guessesUsed");
 let jumboText = document.querySelector("#jumboText");
+let newGameDiv = document.querySelector("#newGameDiv");
+
+// set event listeners
 let ltrInput = document
   .querySelector("#inputLetter")
   .addEventListener("keyup", getKey);
@@ -18,70 +24,123 @@ let ltrBlur = document
   .querySelector("#inputLetter")
   .addEventListener("blur", instructAway);
 const letters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z"
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
 ];
 
 function gameInstruct() {
   if (guessesLeft > 1) {
+    // display instructions
     jumboText.textContent = "Please enter a letter below!";
+  } else {
+    // additional text when game over and box is clicked
+    jumboText.textContent = "GAME OVER!";
+  }
+}
+
+function gameReset() {
+  wins = 0;
+  losses = 0;
+  guessesLeft = 10;
+  guessesUsed = 0;
+  winText.textContent = wins;
+  lossText.textContent = losses;
+  guessUsedText.textContent = guessesUsed;
+  guessLeftText.textContent = guessesLeft;
+  document.querySelector("#stats").style.display = "block";
+  document.querySelector("#inputLetter").removeAttribute("disabled");
+  jumboText.textContent = "Please enter a letter below!";
+}
+
+function createButton() {
+  // create new game button
+  const newButton = document.createElement("button");
+  newButton.classList = "btn btn-block btn-primary mt-3";
+  newButton.id = "newGame";
+  newButton.appendChild(document.createTextNode("New Game"));
+  newGameDiv.append(newButton);
+  let newGameBtn = document
+    .querySelector("#newGame")
+    .addEventListener("click", newGame);
+}
+
+function newGame(e) {
+  newGameDiv.removeChild(e.target);
+  gameReset();
+}
+
+function instructAway() {
+  // no real function - just looks nice
+  if (guessesLeft > 1) {
+    jumboText.textContent = "";
   } else {
     jumboText.textContent = "GAME OVER!";
   }
 }
 
-function instructAway() {
-  jumboText.textContent = "";
-}
-
 function getKey(e) {
   guess = e.key;
   e.target.value = "";
-  game();
+  // validate input as lowercase letter
+  if (guess.charCodeAt() > 96 && guess.charCodeAt() < 123) {
+    game();
+  } else {
+    // message to user
+    jumboText.textContent = "Please enter a lowercase letter to continue.";
+  }
 }
+
+// Game code
 function game() {
+  // Check if have guesses left
   if (guessesLeft > 1) {
-    let targetLetter = letters[Math.floor(Math.random() * 26)];
-    if (guess.toUpperCase() === targetLetter) {
+    // Assign random letter
+    targetLetter = letters[Math.floor(Math.random() * 26)];
+    // check if win
+    if (guess.toLowerCase() === targetLetter) {
+      // update counts and jumbotron text
       wins++;
       winText.textContent = wins;
       jumboText.textContent = "You won!";
     } else {
+      // if loss, update counts and jumbotron text
       losses++;
       guessesUsed++;
       guessesLeft--;
-      jumboText.textContent = `OOPS! The correct guess would have been ${targetLetter}`;
+      jumboText.textContent = `OOPS! The correct guess would have been "${targetLetter.toLowerCase()}"`;
       lossText.textContent = losses;
       guessUsedText.textContent = guessesUsed;
       guessLeftText.textContent = guessesLeft;
     }
+    // end game
   } else {
     jumboText.textContent = "GAME OVER!";
     document.querySelector("#stats").style.display = "none";
-    // document.querySelector("#inputLetter").setAttribute("disabled", "");
+    createButton();
+    document.querySelector("#inputLetter").setAttribute("disabled", "");
   }
 }
